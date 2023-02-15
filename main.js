c59d83d79f555c4cce4b6e9760b30c12f0e67002
fn-Drew <alutter03@gmail.com>
2023-02-14 19:46:34 -0500

    force sphere to aspect ratio

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import './style.css'
 
 // Scene
 const scene = new THREE.Scene();
@@ -10,6 +11,12 @@ const material = new THREE.MeshStandardMaterial({
     color: "#00ff83",
 })
 
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+}
+
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -19,12 +26,28 @@ light.position.set(0, 10, 10)
 scene.add(light)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600)
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height)
 camera.position.z = 10
 scene.add(camera)
 
 // Renderer
 const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({ canvas })
-renderer.setSize(800, 600)
+renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+window.addEventListener('resize', () => {
+    // Update
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    // Update Camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+    renderer.setSize(sizes.width, sizes.height)
+})
+
+const loop = () => {
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(loop)
+}
+loop()
